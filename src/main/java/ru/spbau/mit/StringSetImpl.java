a7cb1a8c677eb03ae1eb8fcea0f0bb6f0d237387
LabutinIgor <Labutin.IgorL@gmail.com>
2015-09-22 01:54:56 +0300

    First version

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,7 +1,115 @@
 package ru.spbau.mit;
 
-/**
- * Created by igorl on 21.09.15.
- */
-public class StringSetImpl {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class StringSetImpl implements StringSet, StreamSerializable {
+    private class Node {
+        int next[] = new int[128];
+        int cnt_down = 0;
+    }
+    private ArrayList<Node> a = new ArrayList<>();
+    private int size = 0;
+
+    StringSetImpl() {
+        a.add(new Node());
+    }
+
+    public boolean add(String element) {
+        if (contains(element))
+            return false;
+
+        int v = 0;
+        a.get(0).cnt_down++;
+        for (int i = 0; i < element.length(); i++) {
+            int u = a.get(v).next[element.charAt(i) - 'A'];
+            if (u == 0) {
+                u = a.size();
+                a.get(v).next[element.charAt(i) - 'A'] = u;
+                a.add(new Node());
+            }
+            a.get(u).cnt_down++;
+            v = u;
+        }
+        size++;
+        return true;
+    }
+
+    public boolean contains(String element) {
+        int v = 0;
+        for (int i = 0; i < element.length(); i++) {
+            int c = element.charAt(i) - 'A';
+            v = a.get(v).next[c];
+            if (v == 0 || a.get(v).cnt_down == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean remove(String element) {
+        if (!contains(element))
+            return false;
+
+        int v = 0;
+        a.get(0).cnt_down--;
+        for (int i = 0; i < element.length(); i++) {
+            v = a.get(v).next[element.charAt(i) - 'A'];
+            a.get(v).cnt_down--;
+        }
+        size--;
+        return true;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int howManyStartsWithPrefix(String prefix) {
+        int v = 0;
+        for (int i = 0; i < prefix.length(); i++) {
+            int c = prefix.charAt(i) - 'A';
+            v = a.get(v).next[c];
+            if (v == 0 || a.get(v).cnt_down == 0) {
+                return 0;
+            }
+        }
+        return a.get(v).cnt_down;
+    }
+
+    public void serialize(OutputStream out) throws SerializationException {
+        try {
+            out.write(size);
+            out.write(a.size());
+            for (Node el : a) {
+                out.write(el.cnt_down);
+                for (int j = 0; j < 128; j++) {
+                    out.write(el.next[j]);
+                }
+            }
+            out.close();
+        } catch (IOException e) {
+            throw new SerializationException();
+        }
+    }
+
+    public void deserialize(InputStream in) throws SerializationException {
+        try {
+            size = in.read();
+            int n = in.read();
+            for (int i = 0; i < n; i++) {
+                a.add(new Node());
+                a.get(i).cnt_down = in.read();
+                for (int j = 0; j < 128; j++) {
+                    a.get(i).next[j] = in.read();
+                }
+            }
+            in.close();
+        } catch (IOException e) {
+            throw new SerializationException();
+        }
+    }
+
 }
