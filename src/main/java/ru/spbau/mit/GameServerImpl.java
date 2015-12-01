721f8dbf0de1c5e885a8cd3dde6aa58f5cc8014d
LabutinIgor <Labutin.IgorL@gmail.com>
2015-12-01 22:26:13 +0300

    Fixed GameServerImpl

--- src/main/java/ru/spbau/mit/GameServerImpl.java
+++ src/main/java/ru/spbau/mit/GameServerImpl.java
@@ -2,12 +2,53 @@ package ru.spbau.mit;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 public class GameServerImpl implements GameServer {
     private final Game plugin;
     private int lastId = 0;
-    private final Map<String, Connection> connections = new HashMap<>();
+    private final Map<String, ConnectionTask> tasks = new HashMap<>();
+
+    private class ConnectionTask implements Runnable {
+        private final Connection connection;
+        private final String id;
+        private final Queue<String> messagesToSend = new ConcurrentLinkedQueue<>();
+        public ConnectionTask(Connection connection, String id) {
+            this.connection = connection;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            plugin.onPlayerConnected(id);
+            while (!connection.isClosed()) {
+                try {
+                    synchronized (connection) {
+                        if (!connection.isClosed()) {
+                            String message = connection.receive(100);
+                            if (message != null) {
+                                plugin.onPlayerSentMsg(id, message);
+                            }
+                        }
+                        if (!connection.isClosed()) {
+                            synchronized (messagesToSend) {
+                                if (!messagesToSend.isEmpty()) {
+                                    connection.send(messagesToSend.poll());
+                                }
+                            }
+                        }
+                    }
+                } catch(InterruptedException e){
+                    break;
+                }
+            }
+            synchronized (tasks) {
+                tasks.remove(id);
+            }
+        }
+    }
+
     public GameServerImpl(String gameClassName, Properties properties) throws ClassNotFoundException, NoSuchMethodException,
             IllegalAccessException, InvocationTargetException, InstantiationException {
         this.plugin = (Game) Class.forName(gameClassName).getConstructor(GameServer.class).newInstance(this);
@@ -26,51 +67,32 @@ public class GameServerImpl implements GameServer {
 
     @Override
     public void accept(final Connection connection) {
-        synchronized (connections) {
-            connections.put(String.valueOf(lastId++), connection);
+        ConnectionTask task = new ConnectionTask(connection, String.valueOf(lastId++));
+        synchronized (tasks) {
+            tasks.put(String.valueOf(lastId - 1), task);
         }
-        Runnable task = new Runnable() {
-            String id = String.valueOf(lastId - 1);
-
-            @Override
-            public void run() {
-                plugin.onPlayerConnected(id);
-                while (!connection.isClosed()) {
-                    try {
-                        synchronized (connection) {
-                            if (!connection.isClosed()) {
-                                String message = connection.receive(100);
-                                if (message != null) {
-                                    plugin.onPlayerSentMsg(id, message);
-                                }
-                            }
-                        }
-                    } catch(InterruptedException e){
-                        break;
-                    }
-                }
-                synchronized (connections) {
-                    connections.remove(id);
-                }
-            }
-        };
         connection.send(String.valueOf(lastId - 1));
         new Thread(task).start();
     }
 
     @Override
     public void broadcast(String message) {
-        synchronized (connections) {
-            for (Connection connection : connections.values()) {
-                connection.send(message);
+        synchronized (tasks) {
+            for (ConnectionTask task : tasks.values()) {
+                synchronized (task.messagesToSend) {
+                    task.messagesToSend.add(message);
+                }
             }
         }
     }
 
     @Override
     public void sendTo(String id, String message) {
-        synchronized (connections) {
-            connections.get(id).send(message);
+        synchronized (tasks) {
+            ConnectionTask task = tasks.get(id);
+            synchronized (task.messagesToSend) {
+                task.messagesToSend.add(message);
+            }
         }
     }
 }
